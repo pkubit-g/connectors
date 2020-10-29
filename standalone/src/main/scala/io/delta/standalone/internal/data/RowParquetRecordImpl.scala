5908fd176797a94e28f5132f77e52e948f5a3932
Scott Sandre <scott.sandre@databricks.com>
2020-10-29 11:08:57 -0400

    added proper parquet4s license

--- standalone/src/main/scala/io/delta/standalone/internal/data/RowParquetRecordImpl.scala
+++ standalone/src/main/scala/io/delta/standalone/internal/data/RowParquetRecordImpl.scala
@@ -1,3 +1,32 @@
+// scalastyle:off
+/*
+ * Copyright (c) 2018 Marcin Jakubowski
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * This file contains code from the parquet4s project (original license above).
+ * It contains modifications, which are licensed as follows:
+ */
+// scalastyle:on
+
 /*
  * Copyright (2020) The Delta Lake Project Authors.
  *
@@ -14,7 +43,6 @@
  * limitations under the License.
  */
 
-// TODO licence for parquet4s
 package io.delta.standalone.internal.data
 
 import java.sql.{Date, Timestamp}
