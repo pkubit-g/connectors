0da9bf13678e3b4b8dc8d5533114beeae3c39dc1
Scott Sandre <scott.sandre@databricks.com>
2021-08-02 17:30:14 -0700

    added some comments for future tests

---
+++ standalone/src/test/scala/io/delta/standalone/internal/ConversionUtilsSuite.scala
@@ -0,0 +1,11 @@
+package io.delta.standalone.internal
+
+// scalastyle:off funsuite
+import org.scalatest.FunSuite
+
+class ConversionUtilsSuite extends FunSuite {
+  // scalastyle:on funsuite
+
+  // TODO: test convertAction and convertActionJ
+
+}
