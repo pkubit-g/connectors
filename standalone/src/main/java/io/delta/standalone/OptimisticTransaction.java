b0bb38782fbe75ac834dff10b2cf545d2e697171
Scott Sandre <scott.sandre@databricks.com>
2021-08-04 10:07:24 -0700

    got a very basic test working

--- standalone/src/main/java/io/delta/standalone/OptimisticTransaction.java
+++ standalone/src/main/java/io/delta/standalone/OptimisticTransaction.java
@@ -6,6 +6,6 @@ import io.delta.standalone.operations.Operation;
 import java.util.List;
 
 public interface OptimisticTransaction {
-    long commit(List<Action> actions);
+//    long commit(List<Action> actions);
     long commit(List<Action> actions, Operation op);
 }
