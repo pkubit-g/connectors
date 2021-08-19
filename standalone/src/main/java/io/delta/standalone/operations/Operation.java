608263cc32f3d0ae5acde23705cc3ec75fc3ac56
Scott Sandre <scott.sandre@databricks.com>
2021-08-19 13:12:31 -0700

    wip; updated Operation.java

--- standalone/src/main/java/io/delta/standalone/operations/Operation.java
+++ standalone/src/main/java/io/delta/standalone/operations/Operation.java
@@ -2,16 +2,25 @@ package io.delta.standalone.operations;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 
 public abstract class Operation {
     private final String name;
     private final Map<String, String> jsonEncodedValues;
+    private final Optional<String> userMetadata;
 
     // TODO: any way to use java-equivalent of val parameters: Map[String, Any] ?
 
     public Operation(String name, Map<String, String> jsonEncodedValues) {
         this.name = name;
         this.jsonEncodedValues = jsonEncodedValues;
+        this.userMetadata = Optional.empty();
+    }
+
+    public Operation(String name, Map<String, String> jsonEncodedValues, Optional<String> userMetadata) {
+        this.name = name;
+        this.jsonEncodedValues = jsonEncodedValues;
+        this.userMetadata = userMetadata;
     }
 
     public String getName() {
@@ -19,6 +28,10 @@ public abstract class Operation {
     }
 
     public Map<String, String> getJsonEncodedValues() {
-        return Collections.unmodifiableMap(jsonEncodedValues);
+        return null == jsonEncodedValues ? null : Collections.unmodifiableMap(jsonEncodedValues);
+    }
+
+    public Optional<String> getUserMetadata() {
+        return userMetadata;
     }
 }
