590a8a6f7ff89ddeb715c79a68740c7e7a789728
Scott Sandre <scott.sandre@databricks.com>
2021-08-26 15:04:50 -0700

    revert 1 line change in DataTypeParser

--- standalone/src/main/scala/io/delta/standalone/internal/util/DataTypeParser.scala
+++ standalone/src/main/scala/io/delta/standalone/internal/util/DataTypeParser.scala
@@ -44,7 +44,7 @@ import org.json4s._
 import org.json4s.jackson.JsonMethods._
 import org.json4s.JsonAST.JValue
 
-private[standalone] object DataTypeParser {
+private[internal] object DataTypeParser {
 
   private val FIXED_DECIMAL = """decimal\(\s*(\d+)\s*,\s*(\-?\d+)\s*\)""".r
 
